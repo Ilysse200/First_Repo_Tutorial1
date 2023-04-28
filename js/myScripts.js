16e2068413a18fbd1c92bf77bbed856d8d53cf2c
Ilysse200 <ilysseted500@gmail.com>
2023-04-28 16:52:39 +0200

    first commmit

---
+++ js/myScripts.js
@@ -0,0 +1,32 @@
+O$(function(){
+    var rate = {"USD":1030, "EURO":1200, "JPY":160};
+    let rateAmount= rate["USD"] ; // get the initial rate amout
+    
+
+    $("#currency-rate").val(rate["USD"]); // initial currency
+
+    $("#select-currency").change(function(){ // when a currency is selected
+        let rateSymbol = $(this).val(); // get the rate symbol
+        rateAmount = rate[rateSymbol]; // overiding variable
+        $("#currency-rate").val(rateAmount);
+    });
+
+    $("#convert").on('click', function(e){
+        e.preventDefault();
+        let amountFrom = $("#amountFrom").val();
+        if(amountFrom == ''){
+            alert("Please add amount to be comverted");
+            return;
+        }
+        converter(amountFrom, rateAmount);
+    });
+
+    function converter(x, y){
+        console.log(x + " " + y)
+        let w = parseInt(x) * parseInt(y);
+        console.log(w);
+        $("#amountTo").val(w);
+    }
+    
+        
+});
